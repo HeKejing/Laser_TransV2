6134ddb25660f45e9d65b65ebcfe5f0bcd293ddc
KejingHe <920477375@qq.com>
2022-05-04 20:42:36 +0800

    2022/05/04 Laser_TransV2 + Calibration Data + Experiment Data

---
+++ generate_sln.bat
@@ -0,0 +1,54 @@
+@echo off
+setlocal
+
+REM This script is used to generate vs2019 solution files
+
+REM determine if camke exists
+where cmake
+if %errorlevel%==0 (
+    echo cmake found
+) else (
+    echo cmake not found
+    goto exit_build
+)
+
+
+REM create build directory
+set dir_name=build
+if exist %dir_name% (
+    echo delete %dir_name%
+    rd /s/q %dir_name%
+)
+md %dir_name%
+echo create %dir_name%
+cd %dir_name%
+
+
+REM cmake
+cmake -G "Visual Studio 16 2019" -A x64 ..
+
+if %errorlevel%==0 (
+    echo.
+    echo.
+    echo sln is generated in the %dir_name%
+    echo.
+    echo.
+) else (
+    echo.
+    echo.
+    echo solution generating is failed!!! Please fix the problem before cmake.
+    echo.
+    echo.
+    goto exit_build
+)
+
+
+
+:build_success
+pause
+exit /b 0
+
+
+:exit_build
+pause
+exit /b 1
